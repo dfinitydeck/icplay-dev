1fea22260c0f76e05eab9aa071c2576f6b6be928
Dean-crypto <whl401765060@gmail.com>
2025-07-06 21:20:04 +0800

    Refactor the ECDSA - related functions. Extract the logic for constructing EcdsaKeyId into an independent function. Optimize the way of parameter passing and add documentation comments to improve readability.

--- canister_platform/src/crypto_utils.rs
+++ canister_platform/src/crypto_utils.rs
@@ -1,43 +1,46 @@
 use ic_cdk::api::management_canister::ecdsa;
 
-#[allow(dead_code)]
+/// Construct EcdsaKeyId
+fn build_key_id(key_name: &str) -> ecdsa::EcdsaKeyId {
+    ecdsa::EcdsaKeyId {
+        curve: ecdsa::EcdsaCurve::Secp256k1,
+        name: key_name.to_string(),
+    }
+}
+
+/// Sign with ECDSA
 pub async fn sign_with(
     key_name: &str,
-    derivation_path: Vec<Vec<u8>>,
-    message_hash: [u8; 32],
+    derivation_path: &[Vec<u8>],
+    message_hash: &[u8; 32],
 ) -> Result<Vec<u8>, String> {
     let args = ecdsa::SignWithEcdsaArgument {
         message_hash: message_hash.to_vec(),
-        derivation_path,
-        key_id: ecdsa::EcdsaKeyId {
-            curve: ecdsa::EcdsaCurve::Secp256k1,
-            name: key_name.to_string(),
-        },
+        derivation_path: derivation_path.to_vec(),
+        key_id: build_key_id(key_name),
     };
 
     let (response,): (ecdsa::SignWithEcdsaResponse,) = ecdsa::sign_with_ecdsa(args)
         .await
-        .map_err(|err| format!("sign_with_ecdsa failed {:?}", err))?;
+        .map_err(|err| format!("sign_with_ecdsa failed: {:?}", err))?;
 
     Ok(response.signature)
 }
 
+/// Get ECDSA public key
 pub async fn public_key_with(
     key_name: &str,
-    derivation_path: Vec<Vec<u8>>,
+    derivation_path: &[Vec<u8>],
 ) -> Result<ecdsa::EcdsaPublicKeyResponse, String> {
     let args = ecdsa::EcdsaPublicKeyArgument {
         canister_id: None,
-        derivation_path,
-        key_id: ecdsa::EcdsaKeyId {
-            curve: ecdsa::EcdsaCurve::Secp256k1,
-            name: key_name.to_string(),
-        },
+        derivation_path: derivation_path.to_vec(),
+        key_id: build_key_id(key_name),
     };
 
     let (response,): (ecdsa::EcdsaPublicKeyResponse,) = ecdsa::ecdsa_public_key(args)
         .await
-        .map_err(|err| format!("ecdsa_public_key failed {:?}", err))?;
+        .map_err(|err| format!("ecdsa_public_key failed: {:?}", err))?;
 
     Ok(response)
 }
